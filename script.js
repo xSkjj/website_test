3671fc4fdf8a5e4529aede0a256cdf88d7038c77
xSkjj <xSkj@mail.de>
2020-09-05 22:46:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-$(document).mousemove(function(e){
+/*$(document).mousemove(function(e){
     $("line").attr({
         x2: e.pageX,
         y2: e.pageY
+    })
+})*/
+$(window).mousedown(function(e) {
+    $(window).mousemove(function(f){
+        if (e.pageX > f.pageX) {
+            $("rect").attr({
+                x: f.pageX,
+                width: e.pageX - f.pageX
+            })
+        } else {
+            $("rect").attr({
+                x: e.pageX,
+                width: f.pageX - e.pageX
+            })
+        };
+        if (e.pageY > f.pageY) {
+            $("rect").attr({
+                y: f.pageY,
+                height: e.pageY - f.pageY
+            })
+        } else {
+            $("rect").attr({
+                y: e.pageY,
+                height: f.pageX - e.pageY
+            })
+        }
+    });
+    $(window).mouseup(function(){
+        $("rect").attr({
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0
+        })
     })
 })
