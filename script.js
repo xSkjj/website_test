61add13e975914d96cc25b8626f95779415d094a
xSkjj <xSkj@mail.de>
2020-09-08 16:18:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-// header move in animation
-if ($("input#hideBox").prop("checked")) {
-    $("header h1").animate({fontSize: "3em"}, function(){
-        $("header h1").css("transform","translateY(50%)")
-    })
-} else {
-    $("header h1").css({
-        "transition":"none",
-        "font-size":"3em",
-        "transform":"translateY(50%)"
-    })
-};
-
-// mouseEffect
-$(window).mousemove(function(e){
-    $("#mouseBox line").attr({
-        x2: e.pageX,
-        y2: e.pageY
-    })
+$(function(){
+    lineEffect();
+    clickColor();
+    autoColor();
+    boxEffect();
+    skipAnim()
 });
 
-// mouseBoxEffect
+// header move in animation
+function skipAnim() {
+    if ($("#skipAnim").prop("checked")) {
+        $("header h1").animate({fontSize: "3em"}, function(){
+            $("header h1").css("transform","translateY(50%)")
+        })
+    } else {
+        $("header h1").css({
+            "transition":"none",
+            "font-size":"3em",
+            "transform":"translateY(50%)"
+        })
+    }
+};
+
+// lines that follow the cursor
+function lineEffect() {
+    if ($("#lineEffect").prop("checked")) {
+        $("#clickColor, #autoColor").prop("disabled", false);
+        if (!($("#clickColor").prop("checked")) && !($("#autoColor").prop("checked"))) {
+            $("#setColor").prop("disabled", false)
+        };
+        $(window).mousemove(function(e){
+            $("#mouseBox line")
+            .show()
+            .attr({
+                x2: e.pageX,
+                y2: e.pageY
+            })
+        })
+    } else {
+        $(window).off("mousemove");
+        $("#mouseBox line").hide();
+        $("#clickColor, #autoColor, #setColor").prop("disabled", true)
+    }
+};
+
+// selectBox effect like Windows
 function boxEffect(){
     if ($("#boxEffect").prop("checked")) {
+        $("#hideBox").prop("disabled", false)
         $(document).mousedown(function(f){
             $("#mouseBox rect").attr({
                 x: f.pageX,
@@ -56,7 +81,7 @@ function boxEffect(){
         });
         $(document).mouseup(function(){
             // hide box on mouseup
-            if ($("input#hideBox").prop("checked")) {
+            if ($("#hideBox").prop("checked")) {
                 $("#mouseBox rect").attr({
                     x: 0,
                     y: 0,
@@ -67,45 +92,86 @@ function boxEffect(){
             $(document).off('mousemove')
         })
     } else {
-        $(document).off("mousedown mouseup")
+        $(document).off("mousedown mouseup");
+        $("#hideBox").prop("disabled", true)
     }
 };
-$(document).ready(boxEffect());
 
 // change line and rect color
+function colorchange() {
+    if ($("#lineEffect").prop("checked")) {
+        var r = Math.floor(Math.random()*256),
+            g = Math.floor(Math.random()*256),
+            b = Math.floor(Math.random()*256);
+        $("#mouseBox line, #setColor + label line").css("stroke","rgb("+r+", "+g+","+b+")");
+        $("#mouseBox rect").css({
+            "fill":"rgba("+r+", "+g+","+b+", 0.133)",
+            "stroke":"rgb("+r+", "+g+","+b+")"
+        })
+    }
+};
 // ...after time period
-setInterval(function(){
-    var r = Math.floor(Math.random()*256),
-        g = Math.floor(Math.random()*256),
-        b = Math.floor(Math.random()*256);
-    $("#mouseBox line").css("stroke","rgb("+r+", "+g+","+b+")");
-    $("#mouseBox rect").css({
-        "fill":"rgba("+r+", "+g+","+b+", 0.133)",
-        "stroke":"rgb("+r+", "+g+","+b+")"
-    })
-}, 10000);
+var autoC = null;
+function autoColor() {
+    if ($("#autoColor").prop("checked")) {
+        autoC = setInterval(colorchange, 5000);
+        $("#setColor").prop("disabled", true)
+    } else {
+        autoColorStop();
+        if (!($("#clickColor").prop("checked")) && $("#lineEffect").prop("checked")) {
+            $("#setColor").prop("disabled", false)
+        }
+    }
+};
+function autoColorStop() {
+    clearInterval(autoC)
+};
 // ...with manual click
-$(document).click(function(){
-    var r = Math.floor(Math.random()*256),
-        g = Math.floor(Math.random()*256),
-        b = Math.floor(Math.random()*256);
-    $("#mouseBox line").css("stroke","rgb("+r+", "+g+","+b+")");
+function clickColor(){
+    if ($("#clickColor").prop("checked")) {
+        $(document).click(colorchange);
+        $("#setColor").prop("disabled", true)
+    } else {
+        $(document).off("click");
+        if (!($("#autoColor").prop("checked")) && $("#lineEffect").prop("checked")) {
+            $("#setColor").prop("disabled", false)
+        }
+    }
+};
+
+// set color manually
+if (!($("#clickColor").prop("checked")) && !($("#autoColor").prop("checked"))) {
+    $("#setColor").prop("disabled", false)
+} else {
+    $("#setColor").prop("disabled", true)
+};
+function setColor() {
+    $("#mouseBox line, #setColor + label line").css("stroke",$("#setColor").val());
     $("#mouseBox rect").css({
-        "fill":"rgba("+r+", "+g+","+b+", 0.133)",
-        "stroke":"rgb("+r+", "+g+","+b+")"
+        "fill":$("#setColor").val()+"22",
+        "stroke":$("#setColor").val()
     })
-});
+};
+function unlockColor() {
+    if ($("#lineEffect").prop("checked")) {
+        $("#setColor").prop("disabled", false);
+        $("#clickColor").prop("checked", false);
+        $(document).off("click");
+        $("#autoColor").prop("checked", false);
+        clearInterval(autoC)
+    }
+};
 
 // settings: # or esc
 $("#settings-wrapper").css("display","flex").hide();
 $(window).keyup(function(e){
     if (e.keyCode === 191 || e.keyCode === 27) {
-        $("#settings-wrapper").fadeToggle()
+        toggleSettings();
     }
-});/*
-$("#cfg-btn").click(function(){
-    $("#settings-wrapper").fadeToggle()
-});*/
+});
+function toggleSettings(){
+    $("#settings-wrapper").stop(true).fadeToggle()
+};
 
 // Education - how much percent have I progressed through the 3 years of learning
 function progress(percentage) {
